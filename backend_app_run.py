069ba59b98a237afa0edf0fe94477839cd6afc53
kubzen5 <kuba.jozefacki@gmail.com>
2025-10-03 15:45:04 +0200

    runner for api and base.py

--- backend_app_run.py
+++ backend_app_run.py
@@ -1,1 +1,12 @@
-uv
+import uvicorn
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host=os.getenv("API_HOST", "0.0.0.0"),
+        port=int(os.getenv("API_PORT", "8080")),
+    )
